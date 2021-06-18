a0a9d2c4d1e1d9fde412fd924bb3a265b87ffb81
Nev-habbo <justinb.ugc@gmail.com>
2021-06-19 00:14:58 +0200

    Added databaseController

--- server/src/controller/AuthController.ts
+++ server/src/controller/AuthController.ts
@@ -19,6 +19,8 @@ class AuthController {
         if(player.account) return;
         if(username.length <= 4) return alt.emitClient(player, "Login::Response", "Der Benutzername ist zu kurz. (Mindestens 4 Zeichen)");
         if(password.length <= 6) return alt.emitClient(player, "Login::Response", "Das Passwort ist zu kurz. (Mindestens 6 Zeichen)");
+
+        
     }
 }
 
---
+++ server/src/controller/DatabaseController.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+
+ * OPEN SOURCE GANGWAR PROJECT 
+ * (C) 2021 Nova
+ * By downloading you agree that you never will sell this project/files.
+ */
+import alt from 'alt-server';
+import mysql from 'mysql';
+ 
+class DatabaseController {
+    private pool: mysql.Pool;
+
+    constructor() {
+        this.pool = mysql.createPool({ host: "localhost", user: "gangwar", password: "gangwar", database: "gangwar" });
+    }
+
+    async query(command: string, args: any[] = []) {
+        return new Promise(resolve => {
+            this.pool.query(command, args, (err, result) => {
+                if(err) {
+                    alt.logError(`[DatabaseController] ERROR: ${err}`);
+                    return resolve(null);
+                }
+
+                resolve(result);
+            });
+        });
+    }
+}
+ 
+export default new DatabaseController();
